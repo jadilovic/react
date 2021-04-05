f5b1bfdd5f3a82d8e4f0c3317577e9eb3745585a
jadilovic <adilovic79@yahoo.com>
2021-04-05 08:41:15 +0200

    basic stuff props

--- src/index.js
+++ src/index.js
@@ -4,53 +4,60 @@ import "./index.css";
 
 // Stateless function component
 // Always return JSX
+const firstBook = {
+  author: "Dav Pilkey",
+  title: "Dog Man: Mothering Heights",
+  img:
+    "https://images-na.ssl-images-amazon.com/images/I/51wlUnNtsHL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+};
+
+const secondBook = {
+  author: "Two Hoots Coloring",
+  title: "Dinosaur Coloring Book for Kids",
+  img:
+    "https://images-na.ssl-images-amazon.com/images/I/517lwYMMc5L._SX384_BO1,204,203,200_.jpg",
+};
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        img={firstBook.img}
+      >
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum
+          dolorem qui debitis architecto delectus illum quo cum vitae veniam
+          optio nulla in repellendus, inventore a amet soluta sequi, quam sint!
+        </p>
+      </Book>
+      <Book
+        author={secondBook.author}
+        title={secondBook.title}
+        img={secondBook.img}
+      />
     </section>
   );
 }
 
-const Book = () => {
+const Book = (props) => {
+  console.log(props);
+  const { img, title, author } = props;
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img src={img} alt="childrens book" />
+      <h1>{title}</h1>
+      <h4
+        style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}
+      >
+        {author}
+      </h4>
+      {props.children}
     </article>
   );
 };
 
-const Image = () => {
-  return (
-    <img
-      src="https://images-na.ssl-images-amazon.com/images/I/51wlUnNtsHL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg"
-      alt="childrens book"
-    />
-  );
-};
-
-const Title = () => {
-  return <h1>Dog Man: Mothering Heights</h1>;
-};
-
-const Author = () => {
-  return (
-    <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}>
-      Dav Pilkey
-    </h4>
-  );
-};
 /*
 const Link = () => <a href="#">first</a>;
 
